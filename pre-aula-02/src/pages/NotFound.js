70a7483d66b28c7568685dd8ed9715a1fde15716
fernandacaramico <fernanda.caramico@bandtec.com.br>
2021-05-02 19:45:38 -0300

    all after first axios

--- pre-aula-02/src/pages/NotFound.js
+++ pre-aula-02/src/pages/NotFound.js
@@ -1,33 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-function NotFound() {
-    return(
-        <>
-        <nav>
-        <div class="container">
-            <img class="logo" src="../images/logo.png" alt="Logo"/>
-            <img src="../images/avatar.png" alt="Avatar" class="avatar"/>
-        </div>
-    </nav>
-
-    <div class="container index ">
-        <div class="index-content">
-            <div class="box-text-index">
-                <div class="info-index not-found">
-                    <h1>Oops!</h1>
-                    <h3>A página que você procura não foi encontrada.</h3>
-                </div>
-                <div class="img-index"></div>
-                <button class="btn-green">Voltar</button>
-            </div>     
-            <img src="../images/not-found.png" alt="" class="index-img"/>
-        </div>  
-    </div> 
-    </>  
-    );
-
-=======
 import NavBar from '../components/NavBar';
 import notFound from '../assets/images/not-found.png'
 import Button from '../components/Button';
@@ -52,7 +23,6 @@ function NotFound() {
             </div>
         </>
     );
->>>>>>> c69a59e1662fc2f5b7e711b64999be3916d29b62
 }
 
 export default NotFound;
